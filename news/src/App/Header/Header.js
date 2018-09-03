b0ee88af2f64a1515dc4562877d3579e5b892ceb
west-investor <vlad.rud.2000@gmail.com>
2018-09-03 09:53:15 +0300

    add logo.png Logo.js modified Header.js

--- news/src/App/Header/Header.js
+++ news/src/App/Header/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import Logo from './Logo/Logo';
+
 const Header = () => (
     <div className="header">
         <div className="container">
             <div className="row">
                 <div className="col-lg-12">
-                    sds
+                    <Logo/>
                 </div>
                 <div className="col-lg-12">
-                    sds
+                    {/* <Article/> */}
                 </div>
             </div>
         </div>
---
+++ news/src/App/Header/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logoImg from './logo.png'
+
+
+const Logo = () => (
+    <div className="logo">
+        <a href="#"><img src={logoImg} alt=""/></a>
+    </div>
+)
+
+
+export default Logo 
