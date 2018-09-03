0c5ecdd4e382d5e48d83f2186769779cd0303225
west-investor <vlad.rud.2000@gmail.com>
2018-09-03 09:19:13 +0300

    add Header  , Main , Footer folders with Header.js, Main.js , Footer.js

--- news/src/App/App.js
+++ news/src/App/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
 
+
+const App = () => (
+    <div>
+        <Header/>
+        <Main/>
+        <Footer/>
+    </div>
+)
 
 
 export default App;
