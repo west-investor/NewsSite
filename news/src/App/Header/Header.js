d153a06bc3c54e9adab112b668b1a939170db212
west-investor <vlad.rud.2000@gmail.com>
2018-09-03 09:30:48 +0300

    add header.css and modified header.js

--- news/src/App/Header/Header.js
+++ news/src/App/Header/Header.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Header = () => (
-    <div></div>
+    <div className="header">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    sds
+                </div>
+                <div className="col-lg-12">
+                    sds
+                </div>
+            </div>
+        </div>
+    </div>
 )
 
 
