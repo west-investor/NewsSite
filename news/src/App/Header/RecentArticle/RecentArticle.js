c81998a5179da7d30690514ff905f0f5e8237d8a
west-investor <vlad.rud.2000@gmail.com>
2018-09-05 14:34:08 +0300

    all

--- news/src/App/Header/RecentArticle/RecentArticle.js
+++ news/src/App/Header/RecentArticle/RecentArticle.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './recentArticle.css';
 
 const RecentArticle = () => (
-    <div className="recentArticle"></div>
+    <div className="recentArticle">
+        <div className=""></div>
+    </div>
 )
 
 
