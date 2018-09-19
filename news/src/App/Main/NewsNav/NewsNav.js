1a5e2475ab41842b3f76d2eb68d54c9fbee5260c
west-investor <vlad.rud.2000@gmail.com>
2018-09-19 20:39:45 +0300

    modified NewsNav.js

--- news/src/App/Main/NewsNav/NewsNav.js
+++ news/src/App/Main/NewsNav/NewsNav.js
@@ -7,6 +7,34 @@ const NewsNav = () => (
                 <label for="search">Search</label>
                 <input type="text" id="search" placeholder="enter news name"></input>
             </form>
+            <div className="categories_comments">
+                <div className="switch-comments_categories">
+                    <div className="categories">
+                        Categories
+                    </div>
+                    <div className="comments">
+                        Recent Comments
+                    </div>
+                </div>
+                <div className="all-categories">
+                    <div>Wordpress</div>
+                    <div>Magento</div>
+                    <div>Internet Marketing</div>
+                    <div>eCommerce</div>
+                    <div>Online Security</div>
+                    <div>SEO</div>
+                </div>
+                <div className="news-type">
+                    <div className="recent_popular_achived-news">
+                        <div className="recent-news">Recent</div>
+                        <div className="popular-news">Popular</div>
+                        <div className="achived-news">Achived</div>
+                    </div>
+                    <div className="all-recent">
+
+                    </div>
+                </div>
+            </div>
         </div>
         
     </div>
