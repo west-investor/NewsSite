7b014caa705a7e687f7d3c3e7c5cf880d77ed84f
west-investor <vlad.rud.2000@gmail.com>
2018-09-23 11:34:25 +0300

    install react-router and set browserRouter in index.js

--- news/src/index.js
+++ news/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowseRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 
 
-ReactDOM.render(<BrowseRouter>
+ReactDOM.render(<BrowserRouter>
     <App />
-</BrowseRouter>, document.getElementById('root'));
+</BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
