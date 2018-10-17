1c64452269b4139978837f9ebe49909fba456000
west-investor <vlad.rud.2000@gmail.com>
2018-10-17 10:37:47 +0300

    install react and react-redux libraries and use provider and create store from them

--- news/src/index.js
+++ news/src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import rootReducer from './features/store/root.reducer'
 
 
-ReactDOM.render(<BrowserRouter>
-    <App />
-</BrowserRouter>, document.getElementById('root'));
+const store = createStore(
+    rootReducer
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
